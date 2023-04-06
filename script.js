82e7544f787d5fab157e897e40774b8b355482bb
Andrea Wimberská <awimberska@gmail.com>
2023-04-06 16:58:21 +0200

    přidána animace na x,o

--- script.js
+++ script.js
@@ -8,13 +8,25 @@ const addText = (e) => {
     if (turn === 'x') {
       document.getElementById('turn').innerHTML = 'o';
       turn = 'o';
+      e.target.classList.add('zoom-in');
     } else {
       document.getElementById('turn').innerHTML = 'x';
       turn = 'x';
+      e.target.classList.add('zoom-in');
     }
   }
 };
 
 for (let i = 0; i < td.length; i++) {
   td[i].addEventListener('click', addText);
+  td[i].addEventListener('animationend', function () {
+    this.classList.remove('zoom-in');
+  });
 }
+
+const restartGame = document.getElementById('restart');
+restartGame.addEventListener('click', function (event) {
+  if (!confirm('Opravdu chcete restartovat hru?')) {
+    event.preventDefault();
+  }
+});
